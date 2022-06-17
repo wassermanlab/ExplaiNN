018f3f9b36b5a3fc0410fc20d9c7c3ec0719c230
oriolfornes <oriol.fornes@gmail.com>
2022-06-17 14:54:05 -0700

    updated get_genomes

--- genomes/get-genomes.sh
+++ genomes/get-genomes.sh
@@ -2,6 +2,6 @@
 
 for GENOME in "hg19" "hg38" "mm10"; do
     if ! [ -f ${GENOME}/${GENOME}.fa.sizes ]; then
-        genomepy install -p UCSC -g ./ -r "^chr[\dXYM]{1,2}$" -t 8 -f ${GENOME}
+        genomepy install -p UCSC -g ./ -t 8 -f ${GENOME}
     fi
 done
